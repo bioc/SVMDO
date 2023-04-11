3426b24771ae77bb1f378ea33c3bd0037aeaef3b
robogeno <erhanozer18@gmail.com>
2023-04-12 01:57:03 +0300

    pack_level_man_page_update

--- R/package_level_info.R
+++ R/package_level_info.R
@@ -1,4 +1,4 @@
-#' SVMDO
+#' @title SVMDO
 #' @description Package Description:
 #' It is an easy-to-use GUI using disease information for detecting tumor/normal sample 
 #' discriminating gene sets from differentially expressed genes. Our approach is based on an 
